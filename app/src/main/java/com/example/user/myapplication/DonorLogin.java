bf82986e310538f09964aee37916b78ce94a16de
Hazzatun <hazzatun.jahan.cse@ulab.edu.bd>
2019-04-13 01:52:34 +0600

    My New project for blood bank

---
+++ app/src/main/java/com/example/user/myapplication/DonorLogin.java
@@ -0,0 +1,68 @@
+package com.example.user.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DonorLogin extends AppCompatActivity {
+    EditText mTextUsername;
+    EditText mTextPassword;
+    Button mButtonLogin;
+    TextView mTextViewRegister;
+    DatabaseHelper db;
+    ViewGroup progressView;
+    protected boolean isProgressShowing = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        db = new DatabaseHelper(this);
+        mTextUsername = (EditText)findViewById(R.id.edittext_username);
+        mTextPassword = (EditText)findViewById(R.id.edittext_password);
+        mButtonLogin = (Button)findViewById(R.id.button_login);
+        mTextViewRegister = (TextView)findViewById(R.id.textview_register);
+        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(DonorLogin.this,DonorReg.class);
+                startActivity(registerIntent);
+            }
+        });
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = mTextUsername.getText().toString().trim();
+                String pwd = mTextPassword.getText().toString().trim();
+                Boolean res = db.checkDonor(user, pwd);
+                if(res == true)
+                {
+                    Intent HomePage = new Intent(DonorLogin.this,DonorHome.class);
+                    startActivity(HomePage);
+                }
+                else
+                {
+                    Toast.makeText(DonorLogin.this,"Login Error",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    public void hideProgressingView() {
+        View v = this.findViewById(android.R.id.content).getRootView();
+        ViewGroup viewGroup = (ViewGroup) v;
+        viewGroup.removeView(progressView);
+        isProgressShowing = false;
+    }
+}
+
---
+++ app/src/main/java/com/example/user/myapplication/DonorReg.java
@@ -0,0 +1,64 @@
+package com.example.user.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DonorReg extends AppCompatActivity {
+    DatabaseHelper db;
+    EditText mTextUsername;
+    EditText mTextPassword;
+    EditText mTextCnfPassword;
+    Button mButtonRegister;
+    TextView mTextViewLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        db = new DatabaseHelper(this);
+        mTextUsername = (EditText)findViewById(R.id.edittext_username);
+        mTextPassword = (EditText)findViewById(R.id.edittext_password);
+        mTextCnfPassword = (EditText)findViewById(R.id.edittext_cnf_password);
+        mButtonRegister = (Button)findViewById(R.id.button_register);
+        mTextViewLogin = (TextView)findViewById(R.id.textview_login);
+        mTextViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent LoginIntent = new Intent(DonorReg.this,DonorLogin.class);
+                startActivity(LoginIntent);
+            }
+        });
+
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = mTextUsername.getText().toString().trim();//trim extra space kete day.
+                String pwd = mTextPassword.getText().toString().trim();
+                String cnf_pwd = mTextCnfPassword.getText().toString().trim();
+
+                if(pwd.equals(cnf_pwd)){
+                    long val = db.addDonor(user,pwd);
+                    if(val > 0){
+                        Toast.makeText(DonorReg.this,"You have registered",Toast.LENGTH_SHORT).show();
+                        Intent moveToLogin = new Intent(DonorReg.this,DonorLogin.class);
+                        startActivity(moveToLogin);
+                    }
+                    else{
+                        Toast.makeText(DonorReg.this,"Registration Error",Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                else{
+                    Toast.makeText(DonorReg.this,"Password is not matching",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
